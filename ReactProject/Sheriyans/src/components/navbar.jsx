9e6e6fcb5b0b844c60aec99bb4892a06e8ee786e
ZUBAIR-OP <zubairlegend18@gmail.com>
2026-01-01 12:07:32 +0500

    Refactor Navbar component using Props Drilling

--- ReactProject/Sheriyans/src/components/navbar.jsx
+++ ReactProject/Sheriyans/src/components/navbar.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import 'remixicon/fonts/remixicon.css'
 
-function Navbar() {
+function Navbar(props) {
   return (
     <div className='nav w-full text-[rgb(233,233,233)] flex justify-around items-center mb-10' style={{fontFamily: 'Neue Machina, sans-serif', padding: '10px 30px'}}>
         <div className='left flex flex-col gap-0.5'>
             <div className='flex gap-3 items-center'>
                 <img className='h-10 w-10' src="/images/light-logo_lNzGXRRlQ.webp" alt="Sheriyans Logo" />
                 <div>
-                    <h3 className='text-md'>Sheriyans</h3>
-                    <h3 className='text-md'>Coding School</h3>
+                    <h3 className='text-md'>{props.title}</h3>
+                    <h3 className='text-md'>{props.subtitle}</h3>
                 </div>
             </div>
         </div>
         <div className='rite flex gap-12 items-center text-sm cursor-pointer'>
-            <p>Home</p>
-            <p>Courses</p>
-            <p>Bootcamps</p>
-            <p>Request Callback</p>
-            <p>Classroom</p>
+            <p>{props.p1}</p>
+            <p>{props.p2}</p>
+            <p>{props.p3}</p>
+            <p>{props.p4}</p>
+            <p>{props.p5}</p>
             <i className="ri-notification-3-fill"></i>
             <img className='w-5 h-5 rounded-full object-center' src="vite.svg" alt="Some Icon" />
         </div>
